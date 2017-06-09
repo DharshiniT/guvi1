adfeebd797377b8ae858932b47bf1da94d34ee7d
DharshiniT <tkp.dharshini@gmail.com>
2017-06-09 15:15:03 +0530

    Create number.java

---
+++ number.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class number
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n%2==0)
+		System.out.println("even");
+		
+		else
+		System.out.println("odd");
+	}
+	
+}
